256a6eefe4303c24029bddbc78c21459c87586e4
daftpunk31 <113768573+daftpunk31@users.noreply.github.com>
2024-07-07 19:29:01 +0530

    Update RevNum.java

--- RevNum.java
+++ RevNum.java
@@ -1,14 +1,13 @@
-package java_codes;
 class RevNum {
   public static void main(String[] args) {
+    System.out.print("Enter a number: ");
+    int num = Integer.parseInt(args[0]);
+    int reversed = 0;
 
-    int num = 1234, reversed = 0;
-    
     System.out.println("Original Number: " + num);
 
     // run loop until num becomes 0
     while(num != 0) {
-    
       // get last digit from num
       int digit = num % 10;
       reversed = reversed * 10 + digit;
